32246f2528c2432151ff15de6a70613c8fa1d32a
kjphelps90 <87020287+kjphelps90@users.noreply.github.com>
2021-09-02 09:37:18 -0400

    added gets to html files on server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const api = require('./routes/index.js');
+const fs = require("fs");
 
 const PORT = process.env.port || 3001;
 
@@ -11,5 +12,20 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/api', api);
 
 app.use(express.static('public'));
+
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+
+
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
 
 
